3e2893933c6b45aac5f0294e21bed9dee20a5635
elihart <konakid@gmail.com>
2013-12-25 19:18:31 -1000

    Weekly image update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219005307) do
+ActiveRecord::Schema.define(version: 20131226051701) do
 
   create_table "albums", force: true do |t|
     t.boolean  "nsfw",         default: false
