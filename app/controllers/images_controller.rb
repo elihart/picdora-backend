071acfd8f834def47725b41e4c05df35cc563e4a
elihart <konakid@gmail.com>
2013-12-29 10:42:32 -1000

    Image routes

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -15,14 +15,45 @@ class ImagesController < ApplicationController
 
   def top
     count = params[:count]
-    category = Category.find(params[:category_id])
+    category_ids = params[:category_ids]
     excludeIds = params[:exclude]
-    if !excludeIds
-      excludeIds = []
+
+    # Convert true/false string param to boolean
+    gif = params[:gif]
+    unless gif.nil?
+      gif = params[:gif].downcase == "true" ? true : false
     end
 
-    images = Image.where(category: category).where.not(id: excludeIds).order(reddit_score: :desc).limit(count)
+
+    images = Image.where(category_id: category_ids).where.not(id: excludeIds)
+
+    images = images.where(gif: gif) unless gif.nil?
+
+    images = images.order(reddit_score: :desc).limit(count)
 
     render json: images.as_json(only: [:id, :imgurId, :reddit_score, :nsfw, :gif, :category_id])
+  end
+
+  def count
+    count = params[:count]
+    category_ids = params[:category_ids]
+
+    # Convert true/false string param to boolean
+    gif = params[:gif]
+
+    result = 0
+
+    if gif.nil?
+      result = Image.where(category_id: category_ids).count
+    elsif gif.downcase == "true"
+      result = Image.where(category_id: category_ids, gif: true).count
+    elsif gif.downcase == "false"
+      result = Image.where(category_id: category_ids, gif: false).count
+    else
+      # If
+      result = Image.where(category_id: category_ids).count
+    end
+
+    render json: {count: result}
   end
 end
