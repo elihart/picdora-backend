0c5123c3576696c38a5505faf935edad19fb24bd
Eli Hart <konakid@gmail.com>
2014-03-08 11:25:30 -0600

    imge update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140301130938) do
+ActiveRecord::Schema.define(version: 20140308172433) do
 
   create_table "albums", force: true do |t|
     t.boolean  "nsfw",         default: false
