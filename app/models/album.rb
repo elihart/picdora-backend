763865beac0b8e54496fc0ab1c74aeeb18de7ca7
Eli Hart <konakid@gmail.com>
2014-04-20 14:38:39 -0600

    Add new images

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
   has_many :images
-  has_and_belongs_to_many :categories, uniq: true
+  has_and_belongs_to_many :categories
 
   validates :imgurId, uniqueness: {case_sensitive: true}
   validates :reddit_score, :imgurId, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many :images, uniq: true
-  has_and_belongs_to_many :albums, uniq: true
+  has_and_belongs_to_many :images
+  has_and_belongs_to_many :albums
 
   validates :name, uniqueness: {case_sensitive: false}
   validates :name, presence: true
